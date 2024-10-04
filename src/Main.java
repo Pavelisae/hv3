ffdb75ec5a3e560dee43445ad5e2b0d79046fe18
isaevpasha <isaevpasa129@gmail.com>
2024-10-04 12:09:49 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 27;
+        System.out.println("Значение переменной а с типом int равно " + a);
+        byte b = 30;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+
+        short s = 2;
+        long l = 4;
+        float f = 9.3f;
+        double d = 7;
+
+     //   задача 2
+        double dd = 27.12;
+        long bb = 987678965549L;
+        double abc = 2.786;
+        int c = 569;
+        int cc = -159;
+        int h = 27897;
+        int g = 67;
+
+    }
+}
